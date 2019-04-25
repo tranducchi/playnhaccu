e8b1f24e439596ecea470cc12ffa922e89e05206
tranducchi <chitranduc9@gmail.com>
2019-04-25 10:09:29 +0700

    ff

--- resources/views/front-end/navbar.blade.php
+++ resources/views/front-end/navbar.blade.php
@@ -78,7 +78,7 @@
 <script>
     $(document).ready(function(){
     
-        $('#key').change(function(){ 
+        $('#key').keyup(function(){ 
             var query = $(this).val();
             if(query != '')
             {
